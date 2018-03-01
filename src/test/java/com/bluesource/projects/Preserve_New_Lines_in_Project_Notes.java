26cd8a3648edaeca82396698aac8586e05725b67
david.grayson <david.grayson@orasi.com>
2018-03-01 16:45:18 -0500

    Test for project notes preserving new lines when saved done and passing Created ProjectNoteForm for the Project note creation form added following methods to Accounts: + clickCreateNote + verifyNoteCreated + getNumberOfNotes

---
+++ src/test/java/com/bluesource/projects/Preserve_New_Lines_in_Project_Notes.java
@@ -0,0 +1,89 @@
+package com.bluesource.projects;
+
+import com.orasi.bluesource.Accounts;
+import com.orasi.bluesource.Header;
+import com.orasi.bluesource.LoginPage;
+import com.orasi.bluesource.ProjectNoteForm;
+import com.orasi.utils.TestReporter;
+import com.orasi.web.WebBaseTest;
+import org.testng.ITestContext;
+import org.testng.annotations.*;
+
+public class Preserve_New_Lines_in_Project_Notes extends WebBaseTest {
+	@BeforeMethod
+	@Parameters({ "runLocation", "browserUnderTest", "browserVersion",
+			"operatingSystem", "environment" })
+	public void setup(@Optional String runLocation, String browserUnderTest,
+					  String browserVersion, String operatingSystem, String environment) {
+		setApplicationUnderTest("BLUESOURCE");
+		setBrowserUnderTest(browserUnderTest);
+		setBrowserVersion(browserVersion);
+		setOperatingSystem(operatingSystem);
+		setRunLocation(runLocation);
+		setEnvironment(environment);
+		setThreadDriver(true);
+		testStart("Preserve New Lines in Project Notes");
+	}
+
+	@AfterMethod
+	public void close(ITestContext testResults){
+		endTest("TestAlert", testResults);
+	}
+
+	@Test
+	public void preserveNewLinesInProjectNotes(){
+		//Test Variables
+		String strAccount = "Account1";
+		String strProject = "Project1";
+		String strNote = "test\ntest test";
+		int numOfNotes = 0;
+
+		//Page Models
+		LoginPage loginPage = new LoginPage(getDriver());
+		Header header = new Header(getDriver());
+		Accounts accounts = new Accounts(getDriver());
+		ProjectNoteForm projectNoteForm = new ProjectNoteForm(getDriver());
+
+		TestReporter.logStep("Logging in as Admin");
+		loginPage.AdminLogin();
+
+		TestReporter.logStep("Navigating to Accounts page");
+		header.navigateAccounts();
+
+		TestReporter.logStep("Clicking " + strAccount + " account link");
+		accounts.clickAccountLink(strAccount);
+
+		TestReporter.logStep("Clicking " + strProject + " project link");
+		accounts.clickProjectLink(strProject);
+
+		TestReporter.logStep("Checking if the project already has notes");
+		if (accounts.verifyNoteCreated()){
+			numOfNotes = accounts.getNumberOfNotes();
+			TestReporter.logStep("The project has " + numOfNotes + " notes");
+		} else {
+			TestReporter.logStep("The project has no notes");
+		}
+
+		TestReporter.logStep("Clicking the Note button");
+		accounts.clickCreateNote();
+
+		TestReporter.logStep("Clicking the Add Note Button");
+		projectNoteForm.clickAddNote();
+
+		TestReporter.logStep("Setting note text");
+		projectNoteForm.setNoteText(strNote);
+
+		TestReporter.logStep("Clicking the create note button");
+		projectNoteForm.clickCreateNote();
+
+		TestReporter.assertTrue(accounts.verifyNoteCreated(),"Verifying note creation badge exists");
+
+		if (numOfNotes > 0)
+			TestReporter.assertTrue(numOfNotes < accounts.getNumberOfNotes(),"Verifying note creation badge incremented");
+
+		TestReporter.logStep("Clicking the Note button");
+		accounts.clickCreateNote();
+
+		TestReporter.assertEquals("test\ntest test",projectNoteForm.getNoteText(),"Verifying that the stored note is equal to the original");
+	}
+}
